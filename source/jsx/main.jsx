df771c0dbf471affdbb0c87dc165e3dbd1a50652
hogesuke <miyado@gmail.com>
2015-09-03 08:34:53 +0900

    デバッグコメントを削除

--- source/jsx/main.jsx
+++ source/jsx/main.jsx
@@ -3,9 +3,6 @@ import PubSub from 'pubsub-js';
 import PlayList from './playlist';
 import ArticleList from './articlelist';
 
-console.debug('PlayList', PlayList);
-console.debug('ArticleList', ArticleList);
-
 React.render(
   <div>
     <PlayList />
