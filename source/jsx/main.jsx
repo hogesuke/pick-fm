2dcbbacfbf385c63ee004762bc93f17867bfd10d
hogesuke <miyado@gmail.com>
2015-09-06 23:58:09 +0900

    reducerの追加

--- source/jsx/main.jsx
+++ source/jsx/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import Redux, { Provider } from 'redux';
 import App from './app';
+import pickApp from './reducers'
+
+let store = Redux.createStore(pickApp);
 
 React.render(
   <div>
