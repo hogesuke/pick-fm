cc158dfff6987341cbde1831f06e8583f67feeff
hogesuke <miyado@gmail.com>
2015-10-31 22:48:39 +0900

    mapping.json, data_importer.rb: csvのカラムに合わせるように修正

--- elasticsearch/data_importer.rb
+++ elasticsearch/data_importer.rb
@@ -1,23 +1,34 @@
 #!/usr/bin/env ruby
 require "csv"
 
+def wrap(str)
+  if str.nil?
+    return '""'
+  end
+  unless str.start_with?('[') and str.end_with?(']')
+    return '"' + str + '"'
+  end
+  str
+end
+
 CSV.open("data/tracks.csv", "r") do |f|
   f.each_with_index do |item, i|
     next if i == 0
     p item
     `curl -XPUT 'http://localhost:9200/pickfm/track/#{item[0]}' -d '
       {
-        "id": "#{item[0]}",
-        "program_name": "#{item[1]}",
-        "personality": "#{item[2]}",
-        "guests_en": "#{item[3]}",
-        "guests_ja": "#{item[4]}",
-        "episode": "#{item[5]}",
-        "tag_en": #{item[6]},
-        "tag_ja": "#{item[7]}",
-        "url": "#{item[8]}",
-        "start_time": "#{item[9]}",
-        "end_time": "#{item[10]}"
+        "id": #{item[0]},
+        "program_name": #{wrap(item[1])},
+        "personality_en": #{wrap(item[2])},
+        "personality_ja": #{wrap(item[3])},
+        "guests_en": #{wrap(item[4])},
+        "guests_ja": #{wrap(item[5])},
+        "episode": #{item[6]},
+        "tag_en": #{wrap(item[7])},
+        "tag_ja": #{wrap(item[8])},
+        "url": #{wrap(item[9])},
+        "start_time": #{item[10]},
+        "end_time": #{item[11]}
       }
     '`
   end
