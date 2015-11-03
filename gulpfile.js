56ac71e4e838c3b9895e0f6da614a2ae9acbaa26
hogesuke <miyado@gmail.com>
2015-11-04 01:38:44 +0900

    package.json: sassの導入その2

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 var webpackConfig = require('./webpack.config.js');
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
 
 gulp.task('cleanBuild', function (cb) {
   var rimraf = require('rimraf');
@@ -13,8 +15,16 @@ gulp.task('build', ['cleanBuild'], function (cb) {
   .pipe(gulp.dest(''));
 });
 
+gulp.task('sass', function() {
+  gulp.src('./source/sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('./www/css'));
+});
+
 gulp.task('watch', function () {
   gulp.watch('./source/jsx/**/*.jsx', ['build']);
+  gulp.watch('./source/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['watch']);
