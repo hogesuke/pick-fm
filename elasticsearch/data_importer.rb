72762692858bde7c70cb93e65c337bc162880d7c
hogesuke <miyado@gmail.com>
2015-11-11 00:24:08 +0900

    data_importer.rb: csvのカラム変更に合わせて修正

--- elasticsearch/data_importer.rb
+++ elasticsearch/data_importer.rb
@@ -17,18 +17,18 @@ CSV.open("data/tracks.csv", "r") do |f|
     p item
     `curl -XPUT 'http://localhost:9200/pickfm/track/#{item[0]}' -d '
       {
-        "id": #{item[0]},
-        "program_name": #{wrap(item[1])},
-        "personality_en": #{wrap(item[2])},
-        "personality_ja": #{wrap(item[3])},
-        "guests_en": #{wrap(item[4])},
-        "guests_ja": #{wrap(item[5])},
-        "episode": #{item[6]},
-        "tag_en": #{wrap(item[7])},
-        "tag_ja": #{wrap(item[8])},
-        "url": #{wrap(item[9])},
-        "start_time": #{item[10]},
-        "end_time": #{item[11]}
+        "id": #{i},
+        "program_name": #{wrap(item[0])},
+        "personality_en": #{wrap(item[1])},
+        "personality_ja": #{wrap(item[2])},
+        "guests_en": #{wrap(item[3])},
+        "guests_ja": #{wrap(item[4])},
+        "episode": #{wrap(item[5])},
+        "tag_en": #{wrap(item[6])},
+        "tag_ja": #{wrap(item[7])},
+        "url": #{wrap(item[8])},
+        "start_time": #{item[9]},
+        "end_time": #{item[10]}
       }
     '`
   end
