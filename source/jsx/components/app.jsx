acca9f8f944579fd71a5d49c623de24f27c90dbe
hogesuke <miyado@gmail.com>
2015-11-23 16:08:12 +0900

    mainのwidthをwindowサイズいっぱいまで設定するように修正

--- source/jsx/components/app.jsx
+++ source/jsx/components/app.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import _ from 'underscore';
+import $ from 'jquery';
 import { connect } from 'react-redux';
 import SideBar from './sidebar';
 import Main from './main';
 
 class App extends Component {
+  getMainWidth() {
+    let $window   = $(window);
+    let window_x  = $window.width();
+    let sidebar_x = 300;
+
+    return window_x - sidebar_x - 20;
+  }
   render() {
     return (
       <div>
         <SideBar />
-        <Main />
+        <Main width={this.getMainWidth()} />
       </div>
     );
   }
--- source/jsx/components/main.jsx
+++ source/jsx/components/main.jsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import SearchBox from './searchbox';
 import TrackList from './tracklist';
-import { fetchTracks } from '../actions'
+import { fetchTracks } from '../actions';
 
 class Main extends Component {
   render() {
     return (
-      <div id="main">
+      <div id="main" style={{ width: this.props.width }}>
         <SearchBox onChange={text => this.props.dispatch(fetchTracks(text))} />
         <TrackList onLoad={text => this.props.dispatch(fetchTracks(text))} />
       </div>
