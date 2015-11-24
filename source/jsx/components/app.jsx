317fc576c50a904a3105a421754179ab163c034e
hogesuke <miyado@gmail.com>
2015-11-24 23:49:58 +0900

    windowサイズを変更したら動的にmainのwidthが変更されるように修正

--- source/jsx/components/app.jsx
+++ source/jsx/components/app.jsx
@@ -6,6 +6,20 @@ import SideBar from './sidebar';
 import Main from './main';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { mainWidth: this.getMainWidth() };
+  }
+  handleResize() {
+    this.setState({ mainWidth: this.getMainWidth() });
+    cosole.debug(this.state.mainWidth);
+  }
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize.bind(this));
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize.bind(this));
+  }
   getMainWidth() {
     let $window   = $(window);
     let window_x  = $window.width();
@@ -17,7 +31,7 @@ class App extends Component {
     return (
       <div>
         <SideBar />
-        <Main width={this.getMainWidth()} />
+        <Main width={this.state.mainWidth} />
       </div>
     );
   }
