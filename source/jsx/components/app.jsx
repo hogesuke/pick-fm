9d6f615ff129a0611f02cefae4c2cc227eaa8b4b
hogesuke <miyado@gmail.com>
2015-11-28 23:21:41 +0900

    app.jsx: resizeに関わる不要な処理を削除

--- source/jsx/components/app.jsx
+++ source/jsx/components/app.jsx
@@ -6,20 +6,6 @@ import SideBar from './sidebar';
 import Main from './main';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { mainWidth: this.getMainWidth() };
-  }
-  handleResize() {
-    this.setState({ mainWidth: this.getMainWidth() });
-    cosole.debug(this.state.mainWidth);
-  }
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize.bind(this));
-  }
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize.bind(this));
-  }
   render() {
     return (
       <div>
