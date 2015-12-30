4cfe1ad299d10947761d3a09797fc09a3b09178f
hogesuke <miyado@gmail.com>
2015-12-30 23:58:26 +0900

    再生ボタンと一時停止ボタンの切り替え（本実装）

--- source/jsx/components/player.jsx
+++ source/jsx/components/player.jsx
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PlayAndPauseButton from './playAndPauseButton';
 
 class Player extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isPaused: false };
-  }
-  getAudioUrl() {
-    let track = this.props.playingTrack;
-    return track.url;
-  }
-  componentDidUpdate() {
-    console.debug('componentDidUpdate');
-    let track = this.props.playingTrack;
+  componentWillReceiveProps(nextProps) {
+    let track = nextProps.playingTrack;
 
     if (!track) return;
 
@@ -22,7 +14,7 @@ class Player extends Component {
 
     let audio = this.audio = new Audio();
     audio.currentTime = track.start_time;
-    audio.src = this.getAudioUrl();
+    audio.src = track.url;
 
     let intervalID = setInterval(() => {
       if (this.isEnd(audio.currentTime)) {
@@ -31,33 +23,24 @@ class Player extends Component {
       }
     }, 500);
 
-    this.playOrPause();
+    audio.play();
   }
   isEnd(currentTime) {
     // todo endedメソッドで判定可能っぽい。後で試してみる
     return this.props.playingTrack.end_time <= currentTime;
   }
-  playOrPause() {
-    console.debug('playOrPause');
-    if (this.audio.paused) {
-      this.audio.play();
-      this.setState({ isPaused: false });
-    } else {
-      this.audio.pause();
-      this.setState({ isPaused: true });
-    }
-  }
   render() {
     let isDisabled = true;
-    if (this.props.playingTrack) {
+    if (this.audio) {
       isDisabled = false;
     }
 
     return (
       <div id="player">
-        <button onClick={this.playOrPause.bind(this)} disabled={isDisabled} className="button play">
-          <i className={this.state.isPaused ? 'fa fa-play' : 'fa fa-pause'}></i>
-        </button>
+        <PlayAndPauseButton
+          audio={ this.audio }
+          isDisabled={ isDisabled }
+        />
       </div>
     );
   }
