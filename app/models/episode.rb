7723d2396055c883981bbf725f72e5861abfc8b6
hogesuke <miyado@gmail.com>
2016-02-14 18:21:36 +0900

    episodeにfind_by_guestスコープを追加

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -4,6 +4,10 @@ class Episode < ActiveRecord::Base
 
   attr_accessor :tracks
 
+  scope :find_by_guest, -> (guest) do
+    joins(:persons).merge(Person.where('name_ja = ? or name_en = ? or nickname = ?', guest, guest, guest))
+  end
+
   def as_json(options={})
     super.as_json(options).merge({
                                      :program => self.program,
