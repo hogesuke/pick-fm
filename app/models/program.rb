e5d9e2a498f2fe83fad800cde3e295908f3a0068
hogesuke <miyado@gmail.com>
2016-02-19 19:50:23 +0900

    program.rb: 不要になったscopeを削除

--- app/models/program.rb
+++ app/models/program.rb
@@ -2,10 +2,6 @@ class Program < ActiveRecord::Base
   has_and_belongs_to_many :persons
   has_many :episodes
 
-  scope :find_with_name, -> (name) do
-    where('name like ?', "%#{name}%")
-  end
-
   def as_json(options={})
     super.as_json(options).merge({:personalities => self.persons})
   end
