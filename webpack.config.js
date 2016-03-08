05293cf00cf3c7572a2ed851f457fc9244396932
hogesuke <miyado@gmail.com>
2016-03-08 21:19:09 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './source/jsx/index.jsx',
-    output: {
-      filename: './www/js/bundle.js'
-    },
-    devtool: 'inline-source-map',
-    module: {
-      loaders: [
-        { test: /\.jsx$/, loader: 'jsx-loader' },
-        { test: /\.jsx$/, loader: 'babel-loader' }
-      ]
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    }
+  entry: './source/jsx/index.jsx',
+  output: {
+    filename: './www/js/bundle.js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      { test: /\.jsx$/, loader: 'jsx-loader' },
+      { test: /\.jsx$/, loader: 'babel-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"development"'
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
 };
