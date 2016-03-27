a909312278272590b3e366f6d578a77f974829e1
hogesuke <miyado@gmail.com>
2016-03-27 18:04:50 +0900

    commentを空でも許容するように修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :episode
 
-  validates :comment, presence: true, length: { in: 1..32 }
+  validates :comment, presence: false, length: { in: 0..32 }
   validates :seconds, presence: true, numericality: {
       only_integer: true,
       greater_than_or_equal_to: 0,
