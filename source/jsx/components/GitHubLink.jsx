b817179f0e4655e9471592133db6265547f78ffc
hogesuke <miyado@gmail.com>
2016-04-06 22:08:57 +0900

    ・githubへのリンクをissuesに変更 ・hover時にballoonを表示するように修正

--- source/jsx/components/GitHubLink.jsx
+++ source/jsx/components/GitHubLink.jsx
@@ -3,14 +3,34 @@ import _ from 'underscore';
 import { connect } from 'react-redux';
 
 class GitHubLink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { visibleBalloon: false };
+  }
+  handleMouseOver() {
+    this.setState({ visibleBalloon: true });
+  }
+  handleMouseOut() {
+    this.setState({ visibleBalloon: false });
+  }
   render() {
+    const { visibleBalloon } = this.state;
+    const balloon = (
+      <div className={ visibleBalloon ? 'balloon' : 'balloon hiding' }>
+        <span>Feedback</span>
+      </div>
+    );
+
     return (
       <a
         id="github-link"
-        href="https://github.com/hogesuke/pick-fm"
+        href="https://github.com/hogesuke/pick-fm/issues"
         target="_blank"
+        onMouseOver={this.handleMouseOver.bind(this)}
+        onMouseOut={this.handleMouseOut.bind(this)}
       >
         <i className="fa fa-github"></i>
+        { visibleBalloon ? balloon : null }
       </a>
     );
   }
