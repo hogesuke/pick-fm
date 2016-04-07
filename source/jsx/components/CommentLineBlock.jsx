1be84299d6f6df015070f2674f7df5cac82b8738
hogesuke <miyado@gmail.com>
2016-04-07 21:46:19 +0900

    コメントが多い場合に省略表示するように修正

--- source/jsx/components/CommentLineBlock.jsx
+++ source/jsx/components/CommentLineBlock.jsx
@@ -6,20 +6,36 @@ import { addComments, removeComment } from '../actions'
 class CommentLineBlock extends Component {
   handleMounseOver() {
     const { dispatch, comments } = this.props;
-    const copiedComments = comments.map((c) => { return Object.assign({}, c); });
-    const heartComments = copiedComments.filter((c) => { return c.comment === ''; });
+    const MAX_COMMENT_COUNT    = 10;
+    const copiedComments       = comments.map((c) => { return Object.assign({}, c); });
+    const heartComments        = copiedComments.filter((c) => { return c.comment === ''; });
     const withoutHeartComments = _.reject(copiedComments, (c) => { return c.comment === ''; });
+    const visibleComments      = _.shuffle(withoutHeartComments).slice(0, MAX_COMMENT_COUNT);
+
+    if (withoutHeartComments.length > MAX_COMMENT_COUNT) {
+      visibleComments.push({
+        comment: `他${ withoutHeartComments.length - MAX_COMMENT_COUNT }件...`,
+        id: 999999,
+        shortened: true
+      });
+    }
 
     if (heartComments.length > 0) {
       heartComments[0].comment = 'heart::count::' + heartComments.length;
-      dispatch(addComments([...withoutHeartComments, heartComments[0]], false));
-    } else {
-      dispatch(addComments(copiedComments, false));
+
+      if (visibleComments.length > 0 && _.last(visibleComments).shortened) {
+        // 省略コメントの前にハートコメントを挿入する
+        visibleComments.splice(visibleComments.length - 1, 0, heartComments[0]);
+      } else {
+        visibleComments.push(heartComments[0]);
+      }
     }
+
+    dispatch(addComments(visibleComments, false));
   }
   handleMounseOut() {
     const { dispatch, comments } = this.props;
-    _.each(comments, (c) => {
+    _.each([...comments, { id: 999999 }], (c) => {
       dispatch(removeComment(c.id));
     });
   }
